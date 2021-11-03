14be49d676e459dbd00708a1c6e61cb189dd288a
Andrew Nelson <Realseal177@gmail.com>
2021-11-03 14:16:19 -0700

    all finished

--- index.js
+++ index.js
@@ -127,11 +127,13 @@ Use the getFlavorByIndex function below to do the following:
   For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 // 2 params - array  / number (holding the place for an index)
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(array, number){
+  array.indexOf(array);
   // return array[0]
+  return array[number];
 }
 
+console.log(getFlavorByIndex(originalFlavors, 9));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, 
